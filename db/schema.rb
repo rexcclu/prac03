8b858acfa961dd7a14c89eae36070c628fde9fe8
Rex Lu <rexcc.lu@gmail.com>
2014-05-10 00:13:30 +0800

    add password to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140508032059) do
+ActiveRecord::Schema.define(version: 20140509160023) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140508032059) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
